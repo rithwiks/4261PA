61c66b1e1459b7d436eabcbf5a0d757db07a50aa
rithwiks <rseth34@gatech.edu>
2024-08-25 16:49:45 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Rithwik Seth on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
