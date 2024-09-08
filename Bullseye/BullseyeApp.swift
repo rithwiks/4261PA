e3bbdc4576f8b9d1ca8cdb98124e12cb303c3d25
rithwiks <rseth34@gatech.edu>
2024-09-08 15:51:34 -0400

    Added Authentification

--- Bullseye/BullseyeApp.swift
+++ Bullseye/BullseyeApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 @main
-struct BullseyeApp: App {
+struct BullseyeApp: SwiftUI.App {
+    init() {
+            let appId = "application-0-lhowrbx"
+            Realm.Configuration.defaultConfiguration = Realm.Configuration(
+                schemaVersion: 1,
+                deleteRealmIfMigrationNeeded: true
+            )
+            _ = App(id: appId)
+        }
     var body: some Scene {
         WindowGroup {
             ContentView()
--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -6,69 +6,210 @@
 //
 
 import SwiftUI
+import RealmSwift
+
+
+public class LeaderBoardItem: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var score: Int = 0
+    @Persisted var ownerId: String
+    @Persisted var username: String
+    convenience init(ownerId: String, score: Int, username: String) {
+        self.init()
+        self.score = score
+        self.ownerId = ownerId
+        self.username = username
+   }
+}
+
 
 struct ContentView: View {
     
     @State var alertIsVisible = false
+    @State private var alertType = false
     @State var sliderValue = 50.0
     @State var target = Int.random(in:1...100)
     @State var score = 0
     @State var currRound = 1
+    let app = RealmSwift.App(id: "application-0-lhowrbx")
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @State private var isAuthenticated = false
+    @State private var isRegistering = false
+    @State private var errorMessage: String?
+    @State var scoreToBeat = 0
+    
+    
     
     var body: some View {
         VStack {
-            VStack {
+            if self.isAuthenticated {
                 VStack {
-                    //Text Row
-                    Spacer()
-                    HStack {
-                        Text("Put the bullseye as close as you can to:")
-                        Text("\(self.target)")
-                    }
-                    Spacer()
-                    
-                    //Slider Row
-                    HStack{
-                        Text("1")
-                        Slider(value: self.$sliderValue, in: 1...100)
-                        Text("100")
-                    }
-                    Spacer()
-                    //Hit Me Row
-                    Button("Hit me") {
-                        print("Button pressed")
-                        self.alertIsVisible = true
-                    }
-                    .alert(isPresented: $alertIsVisible) {
-                        () -> Alert in
-                        return Alert(title: Text(self.alertTitle()), message: Text("My slider's value is \(Int(round(self.sliderValue)))\n You scored \(self.pointsForGuess()) points this round."
-                                                                              ), dismissButton: .default(Text("Awesome")) {
-                            self.score = self.score + self.pointsForGuess()
-                            self.target = Int.random(in:1...100)
-                            self.currRound = self.currRound + 1
-                        })
-                    }
-                    Spacer()
-                    // Score Row
-                    HStack {
-                        Button("Start over") {
-                            score = 0
-                            currRound = 1
-                            target = Int.random(in:1...100)
+                    VStack {
+                        //Text Row
+                        Spacer()
+                        HStack {
+                            Text("Put the bullseye as close as you can to:")
+                            Text("\(self.target)")
                         }
                         Spacer()
-                        Text("Score: ")
-                        Text("\(score)")
-                        Spacer()
-                        Text("Round: ")
-                        Text("\(currRound)")
-                        Spacer()
-                        Button("Info") {
-                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        
+                        //Slider Row
+                        HStack{
+                            Text("1")
+                            Slider(value: self.$sliderValue, in: 1...100)
+                            Text("100")
                         }
-                    }.padding(.bottom, 20)
+                        Spacer()
+                        //Hit Me Row
+                        Button("Hit me") {
+                            print("Button pressed")
+                            alertType = false
+                            self.alertIsVisible = true
+                        }
+                        .alert(isPresented: $alertIsVisible) {
+                            if currRound < 5 {
+                                
+                                return Alert(title: Text(self.alertTitle()), message: Text("The slider's value is \(Int(round(self.sliderValue)))\n You scored \(self.pointsForGuess()) points this round."
+                                                                                          ), dismissButton: .default(Text("Awesome")) {
+                                    self.score = self.score + self.pointsForGuess()
+                                    self.currRound = self.currRound + 1
+                                    self.target = Int.random(in:1...100)
+                                })
+                            } else {
+                                return Alert(title: Text("Leaderboard Submission"), message: Text("The slider's value is \(Int(round(self.sliderValue)))\n You scored \(self.pointsForGuess()) points this round. \nYour score for 5 rounds is \(self.score+self.pointsForGuess())!\n The game will restart!"), dismissButton: .default(Text("Awesome")) {
+                                    self.scoreToBeat = max(self.score+self.pointsForGuess(), self.scoreToBeat)
+                                    self.score = 0
+                                    self.currRound = 1
+                                    self.target = Int.random(in:1...100)
+                                })
+                            }
+                            
+                        }
+                        
+                        Spacer()
+                        // Score Row
+                        HStack {
+                            Button("Start over") {
+                                score = 0
+                                currRound = 1
+                                target = Int.random(in:1...100)
+                            }
+                            Spacer()
+                            Text("Score: ")
+                            Text("\(score)")
+                            Spacer()
+                            Text("Round: ")
+                            Text("\(currRound)")
+                            Spacer()
+                            Text("Score to Beat: ")
+                            Text("\(scoreToBeat)")
+                            Spacer()
+                            Button("Log out", action:logOut)
+                        }.padding(.bottom, 20)
+                    }
+                }
+            } else if isRegistering {
+                TextField("Email", text: $email)
+                                        .autocapitalization(.none)
+                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                        .padding()
+                                    
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                SecureField("Confirm Password", text: $confirmPassword)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button("Register", action:registerSync)
+                    .padding()
+                
+                Button("Cancel", action: {
+                    isRegistering = false
+                })
+                .padding()
+            } else {
+                TextField("Email", text: $email)
+                                        .autocapitalization(.none)
+                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                                        .padding()
+                                    
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button("Log In", action: logIn)
+                    .padding()
+                
+                Button("Create Account", action: {
+                    isRegistering = true
+                })
+                .padding()
+                
+                if let errorMessage = errorMessage {
+                    Text("Error: \(errorMessage)")
+                        .foregroundColor(.red)
                 }
             }
+        }
+    }
+    private func registerSync() {
+        Task {
+            do {
+                try await register()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    private func register() async {
+        guard password == confirmPassword else {
+            errorMessage = "Passwords do not match."
+            return
+        }
+        let credentials = Credentials.emailPassword(email: email, password: password)
+       
+        do {
+            try await app.emailPasswordAuth.registerUser(email: email, password: password)
+            logIn()
+        } catch {
+            print("Failed to register: \(error.localizedDescription)")
+            errorMessage = error.localizedDescription
+        }
+        
+   }
+    
+    private func logIn() {
+        let credentials = Credentials.emailPassword(email: email, password: password)
+        
+        app.login(credentials: credentials) { result in
+            switch result {
+            case .success(let user):
+                print("Successfully logged in as user: \(user)")
+                self.scoreToBeat = 0
+                self.score = 0
+                self.currRound = 1
+                self.target = Int.random(in:1...100)
+                isAuthenticated = true
+            case .failure(let error):
+                print("Failed to log in: \(error.localizedDescription)")
+                errorMessage = error.localizedDescription
+            }
+        }
+    }
+    private func logOut() {
+        guard let user = app.currentUser else { return }
+        
+        user.logOut { error in
+            if let error = error {
+                print("Failed to log out: \(error.localizedDescription)")
+                return
+            }
+            print("Logged out successfully")
+            isAuthenticated = false
         }
     }
     func pointsForGuess() -> Int {
